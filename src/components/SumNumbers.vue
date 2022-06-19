953a110a9c2814529f50ec26246b98ddf4d033e4
mujahid bawazir <msbawazir.c@stc.com.sa>
2022-06-19 14:43:29 +0300

    task

---
+++ src/components/SumNumbers.vue
@@ -0,0 +1,45 @@
+<template>
+  <!--
+ implement the following tasks using Vue js :
+
+  1- move each section to a separate Vue component.
+  2- make sure the functionality is still present after splitting the sections into components.
+  3- implement the Add random number button functionality.
+  4- implement the Reset button functionality .
+
+  note: you should Style/CSS the page to be more presentable.
+  -->
+  <div>
+<h2>Section 1</h2>
+  <p>Sum Value = {{displayNumber}}</p>
+
+
+<h2>Section 2</h2>
+
+<button @click="displayNumber++">Add 1</button>
+<button @click="displayNumber +=10">Add 10</button>
+    <br/>
+<p>implement the the add random number between 1-10 and reset button</p>
+<button>Add a random number between 1-10</button>
+<button>Reset the value to 0</button>
+
+
+
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "sumNumbers",
+  data () {
+    return{
+      displayNumber:0
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/WeatherSaudi.vue
@@ -0,0 +1,28 @@
+<template>
+
+<!--
+  display The current Temperature for 5 saudi cities with the Following Functionality:
+
+1- Creat a table with three columns (city/max temperature/min temperature).
+2- use a Public Weather API to get the values(you can search for and use any Weather API you see fit).
+3- color code the temperature. use Red for 40+ degrees, Yellow for 30-39 and green for 29 and under.
+4- add a button to delete a city from the Table.
+
+
+hint: you can use a vue plug-in to create the table.
+ for the API calls you can use the default vue functions or use Axios plug-in to preform them.
+
+note: you should Style/CSS the page to be more presentable.
+-->
+  <div></div>
+</template>
+
+<script>
+export default {
+  name: "WeatherSaudi"
+}
+</script>
+
+<style scoped>
+
+</style>
